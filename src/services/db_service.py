291ad2585ea8569266b831132037d6898dbd919e
Roman <rs.sku97@gmail.com>
2025-10-03 22:33:58 +0300

    done with some bugs

--- src/services/db_service.py
+++ src/services/db_service.py
@@ -7,10 +7,12 @@ class DbService:
     def __init__(self, repo: DbRepo) -> None:
         self._repo = repo
 
-    async def get_repeat_words(self, chat_id: int) -> list[str]:
+    async def get_repeat_words(self, chat_id: int) -> list[str] | None:
         words = await self._repo.get_repeat_words(chat_id)
+        if not words:
+            return
         return [json.dumps(word_tr) for word_tr in words]
 
     async def save_user_words(self, chat_id: int, words: set[str]) -> None:
         words = [json.loads(word) for word in words]
-        self._repo.save_user_words(chat_id, words)
+        await self._repo.save_user_words(chat_id, words)
